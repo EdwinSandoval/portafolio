44d675ea0a4e31fc0e3b600b3a64f5ed93ed4aa8
Edwin <edwinsandovalvalencia07@gmail.com>
2025-02-18 19:12:22 -0500

    Portfolio actuaizado

--- script.js
+++ script.js
@@ -17,25 +17,37 @@ function seleccionar(){
 }
 //Funcion que aplica las animaciones de las habilidades
 function efectoHabilidades(){
+    console.log("La función efectoHabilidades() se ejecutó")
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        var habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("java");
+        
+        habilidades[1].classList.add("javascript");
+        habilidades[2].classList.add("htmlcss");
+        habilidades[3].classList.add("c");
+        habilidades[4].classList.add("wordpress");
+        habilidades[5].classList.add("angular");
+        habilidades[6].classList.add("react");
+        habilidades[7].classList.add("microservicios");
+        habilidades[8].classList.add("microsoft");
+
+
+        habilidades[9].classList.add("comunicacion");
+        habilidades[10].classList.add("trabajo");
+        habilidades[11].classList.add("creatividad");
+        habilidades[12].classList.add("dedicacion");
+        habilidades[13].classList.add("proyect");
+
     }
+    
 }
 
 
 //detecto el scrolling para aplicar la animacion de la barra de habilidades
 window.onscroll = function(){
     efectoHabilidades();
+    console.log(efectoHabilidades());
+    
 } 
